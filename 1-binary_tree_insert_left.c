39233fbcf4ababf4ce8b40cc2ba460a2b9b9edea
chukssomzzy <chukssomzzy@gmail.com>
2023-02-28 16:04:55 +0100

    ai

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,11 @@
+# include "binary_trees.h"
+
+
+/**
+ * binary_tree_t - inserts a node as the left-child of another node
+ * @parent: parent to the node to insert the left-child in
+ * @value: the value of the node to insert
+ *
+ * Return: a pointer to the newly created node or null if the function fails
+ */
+
