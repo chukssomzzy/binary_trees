b3cffdfae4225c8594a575e1c2f3941df8762be5
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-01 09:15:04 +0100

    base case

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,3 +6,12 @@
  * @func: a pointer to a function to call for each node. The value in
  *			the node must be passed as a parameter to this function.
  */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!func || !tree)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
