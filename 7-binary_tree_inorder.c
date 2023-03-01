fad6b15dd4aac45f1c97af1a11a705df08ec2b14
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-01 09:04:31 +0100

    if func is NULL

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,8 @@
+# include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
+ * @tree: a pointer to the root node of the tree to traverse
+ * @func: a pointer to a function to call for each node. The value in
+ *			the node must be passed as a parameter to this function.
+ */
