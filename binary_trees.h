9f5e6c0bdf0655a7956299f142059fc69368d290
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-01 09:30:06 +0100

    postorder

--- binary_trees.h
+++ binary_trees.h
@@ -28,5 +28,7 @@ int binary_tree_is_leaf(const binary_tree_t *);
 int binary_tree_is_root(const binary_tree_t *);
 void binary_tree_preorder(const binary_tree_t *, void (*)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*)(int));
+void binary_tree_postorder(const binary_tree_t *, void (*)(int));
+
 
 #endif /* _BINARY_TREES_H_ */
