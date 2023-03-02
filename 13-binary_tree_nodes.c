7a6c1e64004c132b6fd1f973f9a05f754154c467
chukssomzzy <chukssomzzy@gmail.com>
2023-03-02 15:20:06 +0100

    balance factor

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -18,5 +18,4 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	num_nodes = (tree->left || tree->right) ? 1 + binary_tree_nodes(tree->left)
 		+ binary_tree_nodes(tree->right) : 0;
 	return (num_nodes);
-
 }
